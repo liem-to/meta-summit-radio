78044ea11351c7b41a3cf9c51a52ea039150a561
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2025-03-24 20:28:10 -0400

    Fixed file hash calculation

--- tools/calc-common.sh
+++ tools/calc-common.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-set -e
-
 : "${file:-?}"
 
 [ -z "${ver}" ] && { echo "Usage: ${0} <version>"; exit 1; }
@@ -15,9 +13,16 @@ prefix="https://files.devops.rfpros.com/builds/linux"
 
 calc_file () {
   ${wget} "${prefix}/${1}/${ver}/${2}" || exit 1
-  echo "SRC_URI[${3}.md5sum] = \"$(md5sum "${2}" | awk '{print $1}')\"" >> "${file}"
-  echo "SRC_URI[${3}.sha256sum] = \"$(sha256sum "${2}" | awk '{print $1}')\"" >> "${file}"
-  rm -f "${2}"
+  
+  dlnd="${tmpdir:-.}/${2}"
+  md5=$(md5sum "${dlnd}" | cut -d ' ' -f 1)
+  sha256=$(sha256sum "${dlnd}" | cut -d ' ' -f 1)
+  rm -f "${dlnd}"
+
+  {
+    echo "SRC_URI[${3}.md5sum] = \"${md5}\""
+    echo "SRC_URI[${3}.sha256sum] = \"${sha256}\""
+  }  >> "${file}"
 }
 
 printf 'RADIO_VERSION = "%s"\n\n' "${ver}" > "${file}"
